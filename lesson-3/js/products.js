65dfe2ee7f90055609820a21d07d8b1dba426973
Tobitolos <olatobiloba58@gmail.com>
2025-02-07 14:05:18 -0500

    completed functions part of lesson

--- lesson-3/js/products.js
+++ lesson-3/js/products.js
@@ -8,19 +8,23 @@
 // STEP 1d: Declare the itemRow and the itemDetail array;
 
 // STEP 2: Build a loop to iterate through the products array
+for (let i = 0, i < products.length; i ++) {
 
 	// STEP 3: Break apart the product name from the price for each item with split()
-
+	products[i] = products[i].split(": ");
 	// STEP 4: Now we have an array as an element of an array - set the second array element to the product price (as type number)
-
+	products[i][1] = Number() 
 	// STEP 5: Add the price of this product to the invoice total
-
+	invoiceTotal += products[i][1];
 	// STEP 6: Capture each product name and price as variables 
-
+	itemDesc = products[i][0];
+	itemPrice = products[i][1];
 	// STEP 7: Create a TR element for this product and price in the invoice table
-
+	itemRow[i] = document.createElement("tr");
 	// STEP 8: Build the string that contains two TD elements each containing one of the item description, and the item price
-
+	itemDetail[i] = `<td>${itemDesc}</td><td>${itemPrice}</td>`
 	// STEP 9: Set the above string as the innerHTML of the new TR element, and then append the new element to the table body (var productList)
-
+	itemRow[i].innerHTML = itemDetail[i];
+	console.log(itemRow[i]);
+};
 // STEP 10: Set the total cost of the invoice as the textContent of the TD in the TFOOT (var totalData), rounding the number to two decimal places
